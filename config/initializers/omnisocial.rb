fd8bfe55271284aad20a9e8b683320de3a4d21cf
Duncan Gough <duncan@suttree.com>
2011-02-28 22:54:15 +0000

    Give me friends birthdays plz. k thx bai

--- config/initializers/omnisocial.rb
+++ config/initializers/omnisocial.rb
@@ -9,12 +9,12 @@ Omnisocial.setup do |config|
   if Rails.env.production?
 
     # Configs for production mode go here
-    config.facebook 'ad49c397eebb9581b3f0a189792ac486', '71d1b7dfd4c607b314fec33a91833c95', :scope => ['publish_stream', 'friends_birthdays', 'friends_email', 'friends_location']
+    config.facebook 'ad49c397eebb9581b3f0a189792ac486', '71d1b7dfd4c607b314fec33a91833c95', :scope => ['publish_stream', 'friends_birthday']
 
   elsif Rails.env.development?
 
     # Configs for development mode go here
-    config.facebook 'ad49c397eebb9581b3f0a189792ac486', '71d1b7dfd4c607b314fec33a91833c95', :scope => ['publish_stream', 'friends_birthdays', 'friends_email', 'friends_location']
+    config.facebook 'ad49c397eebb9581b3f0a189792ac486', '71d1b7dfd4c607b314fec33a91833c95', :scope => ['publish_stream', 'friends_birthday']
 
   end
 
